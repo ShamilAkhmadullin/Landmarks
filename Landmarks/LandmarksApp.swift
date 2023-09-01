1d8f78b73d56f73e6915a9cf851c58852aceffeb
ShamilAkhmadullin <63548889+ShamilAkhmadullin@users.noreply.github.com>
2023-09-01 13:16:18 +1000

    Squashed commit of the following:
    commit adf6bb328e3c958c8688b91223813d2c2dfc7bd7
    Author: ShamilAkhmadullin <63548889+ShamilAkhmadullin@users.noreply.github.com>
    Date:   Fri Sep 1 13:15:17 2023 +1000
    
        Creating and combining views
    
        Implemented the feature: Creating and combining views.

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Akhmadullin, Shamil on 31/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
