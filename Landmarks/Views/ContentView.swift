cd8d1f4021755310e623b00c422dcb30caca06cd
ShamilAkhmadullin <63548889+ShamilAkhmadullin@users.noreply.github.com>
2023-09-02 23:57:12 +1000

    Building list and navigation
    Implemented the feature: Building list and navigation.

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Akhmadullin, Shamil on 31/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
